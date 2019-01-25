b14a8f19dc231d933fd107d869a8c4b7029fa29d
melwinalm <melwinalm@gmail.com>
2019-01-25 15:17:19 +0530

    Removes client and default tasks from gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 const { task } = gulp;
 var nodemon = require('gulp-nodemon');
-var browserSync = require('browser-sync');
 
 const PORT = 8001;
 
@@ -17,14 +16,3 @@ task('server', function (done) {
     })
 })
 
-task('client', function() {
-    browserSync.init({
-        server:{
-            target: "localhost:" + PORT
-        }
-    });
-});
-
-task('default', ['server', 'client'], function(){
-    gulp.watch("*.ejs").on('change', browserSync.reload({stream: true}));
-});
