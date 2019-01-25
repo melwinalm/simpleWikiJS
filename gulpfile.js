0408619a737efc17960b0ed5b4a5311bc0a7399b
melwinalm <melwinalm@gmail.com>
2019-01-25 15:15:30 +0530

    Updates gulpfile to be compatible with version 4.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,30 @@
 var gulp = require('gulp');
+const { task } = gulp;
 var nodemon = require('gulp-nodemon');
 var browserSync = require('browser-sync');
 
 const PORT = 8001;
 
-gulp.task('server', function (done) {
+task('server', function (done) {
     nodemon({
         script: 'server.js',
         ext: 'html css ejs json',
-        env: { 
+        env: {
             'NODE_ENV': 'development',
             'PORT': PORT
-         },
+        },
         done: done
     })
 })
 
-gulp.task('client', function() {
+task('client', function() {
     browserSync.init({
         server:{
             target: "localhost:" + PORT
-        }  
+        }
     });
 });
 
-gulp.task('default', ['server', 'client'], function(){
+task('default', ['server', 'client'], function(){
     gulp.watch("*.ejs").on('change', browserSync.reload({stream: true}));
 });
