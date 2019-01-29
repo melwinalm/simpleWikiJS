bec550ef5359006101d9e7b6d21e464176e11b7c
melwinalm <melwinalm@gmail.com>
2019-01-29 14:14:04 +0530

    Adds config.json validation

--- validate.js
+++ validate.js
@@ -2,6 +2,7 @@ var Ajv = require('ajv');
 var ajv = new Ajv();
 var chalk = require('chalk');
 
+// Validating wiki.json file
 var schemaWiki = {
     type: "object",
     properties: {
@@ -71,6 +72,95 @@ if (!validateWiki) {
         console.log(chalk.red(`- Data Path: ${value.dataPath} Message: ${value.message}`));
     })
 }
-else{
+else {
     console.log(chalk.green("wiki.json: No errors were found in the schema"));
+}
+
+// Validating config.json file
+var schemaConfig = {
+    type: "object",
+    properties: {
+        "main": {
+            type: "object",
+            properties: {
+                "title": {
+                    type: "string",
+                    minLength: 1,
+                }
+            }
+        },
+        "navigation": {
+            type: "object",
+            properties: {
+                "title": {
+                    type: "string",
+                    minLength: 1,
+                },
+                "items": {
+                    type: "array",
+                    items: {
+                        type: "object",
+                        properties: {
+                            "title": {
+                                type: "string",
+                                minLength: 1
+                            },
+                            "url": {
+                                type: "string",
+                                minLength: 1,
+                            }
+                        }
+                    }
+                },
+                "template": {
+                    type: "string",
+                    pattern: "navigation/"
+                }
+            }
+        },
+        "wiki": {
+            type: "object",
+            properties: {
+                "template": {
+                    type: "string",
+                    pattern: "wiki/"
+                },
+                "defaultfolder": {
+                    type: "string",
+                    minLength: 1,
+                }
+            }
+        },
+        "content": {
+            type: "object",
+            properties: {
+                "template": {
+                    type: "string",
+                    pattern: "content/"
+                }
+            }
+        },
+        "footer": {
+            type: "object",
+            properties: {
+                "template": {
+                    type: "string",
+                    pattern: "footer/"
+                }
+            }
+        },
+    }
+}
+
+var dataConfig = require('./config.json');
+
+var validateConfig = ajv.validate(schemaConfig, dataConfig);
+if (!validateConfig) {
+    console.log(chalk.red("config.json: Following errors were found during JSON Schema validation"));
+    ajv.errors.forEach(function (value) {
+        console.log(chalk.red(`- Data Path: ${value.dataPath} Message: ${value.message}`));
+    })
+}
+else {
+    console.log(chalk.green("config.json: No errors were found in the schema"));
 }
